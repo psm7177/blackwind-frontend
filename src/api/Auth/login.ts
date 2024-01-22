c4a63cc1403cba31eb62d4025a47d3fca9514f66
raspberry-pi <raspberry-pi@kakao.com>
2024-01-22 20:02:12 +0900

    Implement the logtin function with fetch api

--- src/api/Auth/login.ts
+++ src/api/Auth/login.ts
@@ -1,3 +1,16 @@
-export const login = async (body:type) => {
-    
+import { API_URL } from "../common"
+
+export const login = async (email: string, password: string) => {
+    return await fetch(API_URL + '/login', {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    }).then(res => res.json());
 }
--- src/components/molecules/Login/Login.tsx
+++ src/components/molecules/Login/Login.tsx
@@ -1,22 +1,45 @@
 "use client";
 import Link from "next/link";
 import HighlightButton from "../../atoms/buttons/HighlightButton";
+import { useState } from "react";
+import { login } from "@/api/Auth/login";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    const { success } = await login(email, password);
+    if (success) {
+      alert("success");
+    } else {
+      alert("failed");
+    }
+  };
+
   return (
     <div className="flex flex-col">
       <div className="text-lg pb-2">BlackWind Service</div>
       <div className="text-sm">Email</div>
-      <input className="rounded-md w-72 border px-2 py-1"></input>
+      <input
+        className="rounded-md w-72 border px-2 py-1"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        value={email}
+      ></input>
       <div className="text-sm">Password</div>
       <input
         className="rounded-md w-72 border px-2 py-1"
         type="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
       ></input>
       <HighlightButton
         className="block w-full"
         onClick={() => {
-          alert("hello world");
+          handleLogin();
         }}
       >
         Login
